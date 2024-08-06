573be861d478b2b49bf5e24a2c842d5531552eb4
Leonhard Breuer <me@leobreuer.dev>
2024-08-06 15:22:52 +0200

    feat(cli): add proper cli using args

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-
+#include <args.hxx>
 #include "gitutil.h"
 
-int main() {
-    GitUtil util;
-    std::cout << util.getVersion() << std::endl;
+int main(int argc, char **argv) {
+    GitUtil gu;
+    args::ArgumentParser parser("git utils", "© 2024 Leonhard Breuer");
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+    args::CompletionFlag completion(parser, {"complete"});
+    // commands
+    args::Group commands(parser, "commands");
+    args::Command version(commands, "version", "generates the next version");
+    // parsing
+    try {
+        parser.ParseCLI(argc, argv);
+        if (version)
+            std::cout << gu.getVersion() << std::endl;
+        else
+            std::cout << "No command submitted" << std::endl;
+    } catch(const args::Completion& e) {
+        std::cout << e.what();
+        return 0;
+    } catch(const args::Help&) {
+        std::cout << parser << std::endl;
+    } catch(const args::ParseError& e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    } catch(const args::ValidationError& e) {
+        std::cout << e.what() << std::endl;
+        std::cout << parser.Help() << std::endl;
+        return 1;
+    }
+    // GitUtil util;
+    // std::cout << util.getVersion() << std::endl;
     return 0;
 }
